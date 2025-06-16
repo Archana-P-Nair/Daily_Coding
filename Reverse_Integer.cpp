ef8e2f2b3f7cbb1e0912ee100f876e752c9ebae7
Archana Prasad Nair <128218198+Archana-P-Nair@users.noreply.github.com>
2025-06-16 20:15:23 +0530

    LeetCode Q.7

---
+++ Reverse_Integer.cpp
@@ -0,0 +1,15 @@
+#include<string>
+class Solution {
+public:
+    int reverse(int n) {
+        long rev=0;
+        while(n!=0){
+            int rem=n%10;
+            rev=rev*10+rem;
+            n=n/10;
+            if(rev>INT_MAX || rev<INT_MIN) return 0;
+            
+        }
+        return (int(rev));
+    }
+};
